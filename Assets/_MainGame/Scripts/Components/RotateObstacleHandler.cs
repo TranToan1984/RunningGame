66fecf3f3bb80cc86f7fbdbbb49f27e40aa60744
TranToan1984 <tranchitoan@gmail.com>
2023-06-11 11:44:03 +0700

    Rotator can rotate according to x,y,z axis

--- Assets/_MainGame/Scripts/Components/RotateObstacleHandler.cs
+++ Assets/_MainGame/Scripts/Components/RotateObstacleHandler.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class RotateObstacleHandler : MonoBehaviour
 {
+    public enum TYPE_ROTATION
+    {
+        AXIS_X,
+
+        AXIS_Y,
+
+        AXIS_Z
+    }
     public float speed = 120f;
+    public TYPE_ROTATION typeRotation = TYPE_ROTATION.AXIS_Y;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Rotate(0, speed * Time.fixedDeltaTime, 0, Space.Self); //rotate Y-axis
+        switch (typeRotation)
+        {
+            case TYPE_ROTATION.AXIS_X:
+                transform.Rotate(speed * Time.fixedDeltaTime, 0, 0, Space.Self); //rotate X-axis
+                break;
+            case TYPE_ROTATION.AXIS_Y:
+                transform.Rotate(0, speed * Time.fixedDeltaTime, 0, Space.Self); //rotate Y-axis
+                break;
+            case TYPE_ROTATION.AXIS_Z:
+                transform.Rotate(0, 0, speed * Time.fixedDeltaTime, Space.Self); //rotate Z-axis
+                break;
+            
+        }
     }
 }
